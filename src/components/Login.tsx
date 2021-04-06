1273ab7276484a1b30a0af6c6857e84eca08b75a
Juan Galarce <j6alarce@gmail.com>
2021-04-06 18:44:00 -0400

    Ejemplo de useReducer, se crea el Authreducer con el metodo de logout por el momento

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,63 @@
+import { useEffect, useReducer } from "react";
+
+// Interfas
+interface AuthState {
+  validate: boolean;
+  token: string | null;
+  username: string;
+  name: string;
+}
+
+// State
+const initialState: AuthState = {
+  validate: true,
+  token: null,
+  username: "",
+  name: "",
+};
+
+// Action
+type AuthAction = {
+  type: "logout";
+};
+
+// Reducer
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case "logout":
+      return {
+        validate: false,
+        token: null,
+        name: "",
+        username: "",
+      };
+    default:
+      return state;
+  }
+};
+
 const Login = () => {
+  const [{ validate }, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "logout" });
+    }, 1500);
+  }, []);
+
+  if (validate) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className="alert alert-info">Validando..</div>
+      </>
+    );
+  }
+
   return (
     <>
-      <h3>Login</h3>
-
-      <div className="alert alert-info">Validando..</div>
-
       <div className="alert alert-danger">No Autenticado</div>
-
       <div className="alert alert-success">Autenticado</div>
-
       <button className="btn btn-primary">Login</button>
       &nbsp;
       <button className="btn btn-danger">Logout</button>
